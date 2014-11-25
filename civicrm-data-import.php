e2cdaae1c190337a00f897058642863877f554bf
John Dorn <john.dorn@gmail.com>
2014-11-25 07:43:38 +0000

    Scraped import map from page because fuck storing that shit the way CiviCRM does

--- civicrm-data-import.php
+++ civicrm-data-import.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once 'civicrm.settings.php';
+$require_base_dir = '/var/www/civicrm/profiles/civicrm_starterkit/modules/civicrm/';
+
+require_once $require_base_dir . 'civicrm.config.php';
  
-require_once 'CRM/Core/Config.php';
-require_once 'CRM/Core/Error.php';
+require_once $require_base_dir . 'CRM/Core/Config.php';
+require_once $require_base_dir . 'CRM/Core/Error.php';
 $config = CRM_Core_Config::singleton( );
  
 civicrm_initialize(TRUE);
@@ -17,63 +19,133 @@ global $civicrm_root;
 
 */
 
+$field_map_from_import = array(
+	'August 2006 Special Ballot Requested' => 'custom_16',
+	'August 2011 Special Ballot Requested' => 'custom_29',
+	'City' => 'city',
+	'City or Village Code' => 'do_not_import',
+	'Current Party Affiliation' => 'custom_35',
+	'Date Registered' => 'custom_50',
+	'February 2005 Special Ballot Requested' => 'custom_13',
+	'February 2006 Special Ballot Requested' => 'custom_17',
+	'February 2007 Special Ballot Requested' => 'custom_20',
+	'Federal Congressional District' => 'custom_37',
+	'Fire' => 'custom_36',
+	'First Name' => 'first_name',
+	'Last Name' => 'last_name',
+	'March 2000 Primary Ballot Requested' => 'custom_2',
+	'March 2004 Primary Ballot Requested' => 'custom_10',
+	'March 2008 Primary Ballot Requested' => 'custom_22',
+	'March 2012 Primary Ballot Requested' => 'custom_31',
+	'May 2001 Special Ballot Requested' => 'custom_4',
+	'May 2002 Primary Ballot Requested' => 'custom_6',
+	'May 2003 Special Ballot Requested' => 'custom_8',
+	'May 2005 Primary Ballot Requested' => 'custom_12',
+	'May 2006 Primary Ballot Requested' => 'custom_15',
+	'May 2007 Primary Ballot Requested' => 'custom_19',
+	'May 2009 Primary Ballot Requested' => 'custom_24',
+	'May 2010 Primary Ballot Requested' => 'custom_26',
+	'May 2011 Primary Ballot Requested' => 'custom_28',
+	'May 2013 Primary Ballot Requested' => 'custom_33',
+	'May 2014 Primary Ballot Requested' => 'custom_34',
+	'Middle Initial' => 'middle_name',
+	'Name Suffix' => 'suffix_id',
+	'November 2000 General Ballot Requested' => 'custom_1',
+	'November 2001 General Ballot Requested' => 'custom_3',
+	'November 2002 General Ballot Requested' => 'custom_5',
+	'November 2003 General Ballot Requested' => 'custom_7',
+	'November 2004 General Ballot Requested' => 'custom_9',
+	'November 2005 General Ballot Requested' => 'custom_11',
+	'November 2006 General Ballot Requested' => 'custom_14',
+	'November 2007 General Ballot Requested' => 'custom_18',
+	'November 2008 General Ballot Requested' => 'custom_21',
+	'November 2009 General Ballot Requested' => 'custom_23',
+	'November 2010 General Ballot Requested' => 'custom_25',
+	'November 2011 General Ballot Requested' => 'custom_27',
+	'November 2012 General Ballot Requested' => 'custom_30',
+	'November 2013 General Ballot Requested' => 'custom_32',
+	'Park' => 'custom_38',
+	'Police' => 'custom_39',
+	'Precinct' => 'custom_40',
+	'Precinct Split' => 'custom_41',
+	'Road' => 'custom_42',
+	'School District' => 'custom_43',
+	'State' => 'state_province',
+	'State House District' => 'custom_44',
+	'State Senate District' => 'custom_45',
+	'Township' => 'custom_46',
+	'Voter Address' => 'street_address',
+	'Voter ID' => 'custom_47',
+	'Voter Status' => 'custom_48',
+	'Year Born' => 'custom_49',
+	'Zip Code' => 'postal_code',
+);
+
 $field_mapping = array(
-  // base fields
-  
+	// base fields
+	'City' => 'base',
+	'City or Village Code' => 'base',
+	'First Name' => 'base',
+	'Last Name' => 'base',
+	'Middle Initial' => 'base',
+	'Name Suffix' => 'base',
+	'State' => 'base',
+	'Voter Address' => 'base',
+	'Zip Code' => 'base',  
 
-  // Voting History
-  'March 2000 Primary Ballot Requested' => 'Voting History',
-  'November 2000 General Ballot Requested' => 'Voting History',
-  'November 2001 General Ballot Requested' => 'Voting History',
-  'May 2001 Special Ballot Requested' => 'Voting History',
-  'November 2002 General Ballot Requested' => 'Voting History',
-  'May 2002 Primary Ballot Requested' => 'Voting History',
-  'November 2003 General Ballot Requested' => 'Voting History',
-  'May 2003 Special Ballot Requested' => 'Voting History',
-  'November 2004 General Ballot Requested' => 'Voting History',
-  'March 2004 Primary Ballot Requested' => 'Voting History',
-  'November 2005 General Ballot Requested' => 'Voting History',
-  'May 2005 Primary Ballot Requested' => 'Voting History',
-  'February 2005 Special Ballot Requested' => 'Voting History',
-  'November 2006 General Ballot Requested' => 'Voting History',
-  'May 2006 Primary Ballot Requested' => 'Voting History',
-  'August 2006 Special Ballot Requested' => 'Voting History',
-  'February 2006 Special Ballot Requested' => 'Voting History',
-  'November 2007 General Ballot Requested' => 'Voting History',
-  'May 2007 Primary Ballot Requested' => 'Voting History',
-  'February 2007 Special Ballot Requested' => 'Voting History',
-  'November 2008 General Ballot Requested' => 'Voting History',
-  'March 2008 Primary Ballot Requested' => 'Voting History',
-  'November 2009 General Ballot Requested' => 'Voting History',
-  'May 2009 Primary Ballot Requested' => 'Voting History',
-  'November 2010 General Ballot Requested' => 'Voting History',
-  'May 2010 Primary Ballot Requested' => 'Voting History',
-  'November 2011 General Ballot Requested' => 'Voting History',
-  'May 2011 Primary Ballot Requested' => 'Voting History',
-  'August 2011 Special Ballot Requested' => 'Voting History',
-  'November 2012 General Ballot Requested' => 'Voting History',
-  'March 2012 Primary Ballot Requested' => 'Voting History',
-  'November 2013 General Ballot Requested' => 'Voting History',
-  'May 2013 Primary Ballot Requested' => 'Voting History',
-  'May 2014 Primary Ballot Requested' => 'Voting History',
+	// Voting History
+	'March 2000 Primary Ballot Requested' => 'Voting History',
+	'November 2000 General Ballot Requested' => 'Voting History',
+	'November 2001 General Ballot Requested' => 'Voting History',
+	'May 2001 Special Ballot Requested' => 'Voting History',
+	'November 2002 General Ballot Requested' => 'Voting History',
+	'May 2002 Primary Ballot Requested' => 'Voting History',
+	'November 2003 General Ballot Requested' => 'Voting History',
+	'May 2003 Special Ballot Requested' => 'Voting History',
+	'November 2004 General Ballot Requested' => 'Voting History',
+	'March 2004 Primary Ballot Requested' => 'Voting History',
+	'November 2005 General Ballot Requested' => 'Voting History',
+	'May 2005 Primary Ballot Requested' => 'Voting History',
+	'February 2005 Special Ballot Requested' => 'Voting History',
+	'November 2006 General Ballot Requested' => 'Voting History',
+	'May 2006 Primary Ballot Requested' => 'Voting History',
+	'August 2006 Special Ballot Requested' => 'Voting History',
+	'February 2006 Special Ballot Requested' => 'Voting History',
+	'November 2007 General Ballot Requested' => 'Voting History',
+	'May 2007 Primary Ballot Requested' => 'Voting History',
+	'February 2007 Special Ballot Requested' => 'Voting History',
+	'November 2008 General Ballot Requested' => 'Voting History',
+	'March 2008 Primary Ballot Requested' => 'Voting History',
+	'November 2009 General Ballot Requested' => 'Voting History',
+	'May 2009 Primary Ballot Requested' => 'Voting History',
+	'November 2010 General Ballot Requested' => 'Voting History',
+	'May 2010 Primary Ballot Requested' => 'Voting History',
+	'November 2011 General Ballot Requested' => 'Voting History',
+	'May 2011 Primary Ballot Requested' => 'Voting History',
+	'August 2011 Special Ballot Requested' => 'Voting History',
+	'November 2012 General Ballot Requested' => 'Voting History',
+	'March 2012 Primary Ballot Requested' => 'Voting History',
+	'November 2013 General Ballot Requested' => 'Voting History',
+	'May 2013 Primary Ballot Requested' => 'Voting History',
+	'May 2014 Primary Ballot Requested' => 'Voting History',
 
-  // Voter Metadata
-  'Current Party Affiliation' => 'Voter Metadata',
-  'Fire District' => 'Voter Metadata',
-  'Federal Congressional District' => 'Voter Metadata',
-  'Park District' => 'Voter Metadata',
-  'Police District' => 'Voter Metadata',
-  'Precinct Partial' => 'Voter Metadata',
-  'Precinct Whole' => 'Voter Metadata',
-  'Road District' => 'Voter Metadata',
-  'School District' => 'Voter Metadata',
-  'State House District' => 'Voter Metadata',
-  'State Senate District' => 'Voter Metadata',
-  'Township' => 'Voter Metadata',
-  'Voter ID' => 'Voter Metadata',
-  'Voter Status' => 'Voter Metadata',
-  'Birth Year' => 'Voter Metadata',
-  'Voter Registration Date' => 'Voter Metadata',
+	// Voter Metadata
+	'Current Party Affiliation' => 'Voter Metadata',
+	'Fire District' => 'Voter Metadata',
+	'Federal Congressional District' => 'Voter Metadata',
+	'Park District' => 'Voter Metadata',
+	'Police District' => 'Voter Metadata',
+	'Precinct Partial' => 'Voter Metadata',
+	'Precinct Whole' => 'Voter Metadata',
+	'Road District' => 'Voter Metadata',
+	'School District' => 'Voter Metadata',
+	'State House District' => 'Voter Metadata',
+	'State Senate District' => 'Voter Metadata',
+	'Township' => 'Voter Metadata',
+	'Voter ID' => 'Voter Metadata',
+	'Voter Status' => 'Voter Metadata',
+	'Birth Year' => 'Voter Metadata',
+	'Voter Registration Date' => 'Voter Metadata',
 );
 
 define('FILE_SKIP_EMPTY_LINES', TRUE);
@@ -135,7 +207,7 @@ function add_record($record) {
     } elseif($dst_field == 'Voter Metadata') {
       array_push($voter_metadata, array('group' => 'Voter Metadata', 'field' => $field, 'value' => $value));
     } elseif($dst_field == 'base') {
-      $contact[$dst_field] = $value;
+      $contact[field_map_from_import[$dst_field]] = $value;
     }
     
     $result = null;
